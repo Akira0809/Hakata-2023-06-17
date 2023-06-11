fa0f7f8707c65da8e0236d5384f020201f570e2a
eiei114 <nowaka37564@gmail.com>
2023-06-11 16:10:02 +0900

    update

--- go_project/main.go
+++ go_project/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,10 +10,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
+func getAPI(w http.ResponseWriter, r *http.Request) {
+	log.Println("ゲットできた")                  // ログを出力
+	fmt.Fprintf(w, "Get request received") // 応答メッセージ
+}
+
 func main() {
 	fmt.Println("Go Test!!")
 
 	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+
+	http.HandleFunc("/get", getAPI)
+
 	http.ListenAndServe(":8080", nil)
 
 }

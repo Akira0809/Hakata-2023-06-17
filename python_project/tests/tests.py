e08d4d8b3337c68aaa5bf593c0d4e3890784504b
student-ops <gogakuec@gmail.com>
2023-06-12 01:14:18 +0900

    add print test

--- python_project/tests/tests.py
+++ python_project/tests/tests.py
@@ -1,8 +1,8 @@
 from src.llama import chat
 
-def test_llama_chat():
+def justPrintLlamaChat():
   q = "愛知県の観光産業についての概要をおしえて"
   for response_text in chat.llama_chat(q):
-    print(response_text)
+    print(response_text, end="", flush=True)
 
-test_llama_chat()
+justPrintLlamaChat()

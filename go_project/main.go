9ccdbe0e47f54eddcbb6baa51264404a691ee9be
student-ops <gogakuec@gmail.com>
2023-06-11 18:22:25 +0900

    とりあえずできた!!

--- go_project/main.go
+++ go_project/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -17,15 +16,38 @@ func getAPI(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Get request received") // 応答メッセージ
 }
 func stream(w http.ResponseWriter, r *http.Request) {
-	flusher, _ := w.(http.Flusher)
-	cw := httputil.NewChunkedWriter(w)
-	for i := 0; i < 3; i++ {
-		cw.Write([]byte("hello"))
-		flusher.Flush()
-		time.Sleep(time.Second)
+	PATH := "http://localhost:5000/llama_chat"
+	resp, err := http.Get(PATH)
+	if err != nil {
+		http.Error(w, "Error from Python API", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+
+	// Create a buffer to store chunks of the response
+	buf := make([]byte, 1024)
+	for {
+		n, err := resp.Body.Read(buf)
+		if err != nil && err != io.EOF {
+			http.Error(w, "Error reading from Python API", http.StatusInternalServerError)
+			return
+		}
+		if n == 0 {
+			break
+		}
+		if _, err := w.Write(buf[:n]); err != nil {
+			http.Error(w, "Error writing to response", http.StatusInternalServerError)
+			return
+		}
+		// Flush the response writer
+		if flusher, ok := w.(http.Flusher); ok {
+			flusher.Flush()
+		}
 	}
 }
-
 func main() {
 	fmt.Println("Go Test!!")
 

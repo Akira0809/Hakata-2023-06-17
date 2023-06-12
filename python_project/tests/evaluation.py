dd22594b8fbb41adc942be47d09837d505c479cb
student-ops <gogakuec@gmail.com>
2023-06-13 04:50:35 +0900

    update test

--- python_project/tests/evaluation.py
+++ python_project/tests/evaluation.py
@@ -5,12 +5,11 @@ from llama_index import(
     load_index_from_storage,
     StorageContext
 )
-from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
 
 
 # build service context
-llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0))
+llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0,max_tokens=1024))
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
 # build index
@@ -24,5 +23,7 @@ evaluator = ResponseEvaluator(service_context=service_context)
 query_engine = index.as_query_engine()
 response = query_engine.query("愛知の観光産業の概要をおしえて")
 print(response)
-eval_result = evaluator.evaluate(response)
+print("####################")
+print(response.source_nodes[0])
+eval_result =  evaluator.evaluate_source_nodes(response)
 print(str(eval_result))

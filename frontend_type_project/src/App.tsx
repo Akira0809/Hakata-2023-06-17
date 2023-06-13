d4540e29537a86a59f7b39655ee690ceb6f63789
eiei114 <nowaka37564@gmail.com>
2023-06-13 23:13:46 +0900

    add 47 prefectures dropdown

--- frontend_type_project/src/App.tsx
+++ frontend_type_project/src/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const prefectures = [
+    '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
+    '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
+    '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県',
+    '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県',
+    '鳥取県', '島根県', '岡山県', '広島県', '山口県',
+    '徳島県', '香川県', '愛媛県', '高知県',
+    '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県',
+];
+
 const App: React.FC = () => {
   return (
       <div>
@@ -11,11 +21,11 @@ const App: React.FC = () => {
           <h2>サイト概要</h2>
           <p>ここにサイトの概要を記述します。</p>
 
-          <select>
-            {Array.from({ length: 47 }, (_, i) => (
-                <option value={i + 1} key={i}>{i + 1}都道府県</option>
-            ))}
-          </select>
+            <select>
+                {prefectures.map((prefecture, i) => (
+                    <option value={prefecture} key={i}>{prefecture}</option>
+                ))}
+            </select>
 
           <button>チャットルームへ</button>
         </main>

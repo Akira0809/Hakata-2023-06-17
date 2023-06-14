3714c0eaf447242b13904d7548ee955e9d4d966c
eiei114 <nowaka37564@gmail.com>
2023-06-14 21:30:09 +0900

    feat Linkを通す

--- frontend_type_project/src/App.tsx
+++ frontend_type_project/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,20 +8,18 @@ import OverView from "./pages/OverView";
 import TermsOfService from "./pages/TermsOfService";
 
 
-
-
 const App: React.FC = () => {
     return (
-        <Router  basename="/static_goto0617">
+        <Router basename="/static_goto0617">
             <div>
                 <Header/>
                 <main>
-                <Routes>
-                    <Route  path="/" element={<HomePage />}/>
-                    <Route path="/chatroom" element={<ChatRoom/>}/>
-                    <Route path="/overview" element={<OverView/>}/>
-                    <Route path="/termsofservice" element={<TermsOfService/>}/>
-                </Routes>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/chatroom" element={<ChatRoom/>}/>
+                        <Route path="/overview" element={<OverView/>}/>
+                        <Route path="/termsofservice" element={<TermsOfService/>}/>
+                    </Routes>
                 </main>
                 <Footer/>
             </div>

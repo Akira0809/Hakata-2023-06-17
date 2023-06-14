a9ac1d194e56e92ba39fc7d6b38c741d1829259e
ray03j <eihouse.potato@outlook.jp>
2023-06-14 21:41:58 +0900

    [add]frontend project

---
+++ frontend_project/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { useState, useRef, useEffect } from 'react'
+// import styles from '../styles/Home.module.css'
+// import ReactMarkdown from 'react-markdown'
+
+function App() {
+  return (
+    <div>
+      <h2>hoge</h2>
+    </div>
+  );
+}
+
+export default App;

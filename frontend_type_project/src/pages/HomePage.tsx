8ecb80efe67035367f8b3ed78716875c3103774e
eiei114 <nowaka37564@gmail.com>
2023-06-14 22:56:48 +0900

    feat 未選択に対応

--- frontend_type_project/src/pages/HomePage.tsx
+++ frontend_type_project/src/pages/HomePage.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import {useState} from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 const prefectures = [
-    '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
+    '未選択', '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
     '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
     '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県',
     '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県',
@@ -12,7 +11,7 @@ const prefectures = [
     '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県',
 ];
 
-const HomePage: React.FC = () =>{
+const HomePage: React.FC = () => {
     const [selectedPrefecture, setSelectedPrefecture] = useState("北海道");
 
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -20,7 +19,7 @@ const HomePage: React.FC = () =>{
         localStorage.setItem('prefecture', event.target.value);
     }
 
-    return(
+    return (
         <div>
             <h2>サイト概要</h2>
             <p>ここにサイトの概要を記述します。</p>

1c28b20b7ccc9c378b1eb2a3318828e629ecfde3
eiei114 <nowaka37564@gmail.com>
2023-06-16 02:39:58 +0900

    Fixed the position of the submit button

--- frontend_type_project/src/pages/ChatRoom.tsx
+++ frontend_type_project/src/pages/ChatRoom.tsx
@@ -52,19 +52,17 @@ const ChatRoom: React.FC = () => {
                     </div>
                 ))}
             </div>
-            <div className="mt-6">
+            <div className="mt-6 flex">
                 <input
                     type="text"
                     value={message}
                     onChange={e => setMessage(e.target.value)}
-                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="flex-grow py-2 px-3 border border-gray-300 bg-white rounded-l-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
-            </div>
-            <div className="mt-4">
                 <button
                     onClick={handleSend}
                     disabled={loading}
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-r-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     {loading ? '...' : '送信'}
                 </button>
             </div>

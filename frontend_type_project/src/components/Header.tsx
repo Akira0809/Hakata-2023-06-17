8ccbc0836d11f0c137c07b8843bd1f4e0868ab35
eiei114 <nowaka37564@gmail.com>
2023-06-15 23:38:45 +0900

    Implement CSS in component

--- frontend_type_project/src/components/Header.tsx
+++ frontend_type_project/src/components/Header.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import '../styles/Header.css';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return (
-        <header className="header">
-            <div className="title">
-                <h1>GoToコンシェルジュ</h1>
-            </div>
-            <nav className="menu">
-                <ul>
-                    <li><Link to="/">ホーム</Link></li>
-                    <li><Link to="/overview">概要</Link></li>
-                    <li><Link to="/termsofservice">規約</Link></li>
-                    <li><Link to="/contact">コンタクト</Link></li>
-                </ul>
-            </nav>
-            <div className="header-right">
-                <Link to="/chatroom">チャットルームへ</Link>
+        <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+                <h1 className="text-3xl font-bold text-gray-900">GoToコンシェルジュ</h1>
+                <nav className="flex">
+                    <ul className="flex space-x-10">
+                        <li><Link className="text-gray-500 hover:text-gray-700" to="/">ホーム</Link></li>
+                        <li><Link className="text-gray-500 hover:text-gray-700" to="/overview">概要</Link></li>
+                        <li><Link className="text-gray-500 hover:text-gray-700" to="/termsofservice">規約</Link></li>
+                        <li><Link className="text-gray-500 hover:text-gray-700" to="/contact">コンタクト</Link></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
 };
 export default Header;
+
 
